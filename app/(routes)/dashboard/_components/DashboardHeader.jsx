11dfddf223a88803c60aa86f49150e76cd421b55
Alfian <muhammadalfiannovanto17@gmail.com>
2024-08-29 23:11:16 +0700

    dashboard

---
+++ app/(routes)/dashboard/_components/DashboardHeader.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function DashboardHeader() {
+  return (
+    <div className="p-5 shadow-sm border-b">
+        
+        <div className="">
+            Search
+        </div>
+    </div>
+  )
+}
+
+export default DashboardHeader
--- app/(routes)/dashboard/_components/SideNav.jsx
+++ app/(routes)/dashboard/_components/SideNav.jsx
@@ -1,52 +1,78 @@
-import { UserButton } from '@clerk/nextjs'
-import {  LayoutDashboard, LucideBanknote, LucideCoffee, PiggyBank, ReceiptText } from 'lucide-react'
-import Image from 'next/image'
-import React from 'react'
+"use client";
+
+import { UserButton } from "@clerk/nextjs";
+import {
+  LayoutDashboard,
+  LucideBanknote,
+  LucideCoffee,
+  PiggyBank,
+  ReceiptText,
+} from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import React, { useEffect } from "react";
 
 function SideNav() {
-    const menuList=[
-        {
-            id:1,
-            name:'Dashboard',
-            Icon: LayoutDashboard
-        },
-        {
-            id:2,
-            name:'Keuangan',
-            Icon: LucideBanknote
-        },
-        {
-            id:3,
-            name:'Pengeluaran',
-            Icon: ReceiptText
-        },
-        // {
-        //     id:4,
-        //     name:'Buy Me Coffee',
-        //     Icon: LucideCoffee
-        // },
-    ]
+  const menuList = [
+    {
+      id: 1,
+      name: "Dashboard",
+      Icon: LayoutDashboard,
+      path: "/dashboard",
+    },
+    {
+      id: 2,
+      name: "Keuangan",
+      Icon: LucideBanknote,
+      path: "/dashboard/budgets",
+    },
+    {
+      id: 3,
+      name: "Pengeluaran",
+      Icon: ReceiptText,
+      path: "/dashboard/expense",
+    },
+    // {
+    //     id:4,
+    //     name:'Buy Me Coffee',
+    //     Icon: LucideCoffee
+    // },
+  ];
 
+  const path = usePathname();
+
+  useEffect(() => {
+    console.log(path);
+  }, [path]);
 
   return (
-    <div className="h-screen p-5 border shadow-sm mb5">
-        <Image src={"/budget.gif"} alt="logo" width={150} height={150} />
-      <span className="text-primary font-extrabold text-3xl ">Cuan Tracker</span>
+    <div className="h-screen p-5 border shadow-sm">
+      <Image src={"/budget.gif"} alt="logo" width={100} height={100} />
+      <span className="text-primary font-extrabold text-3xl ">
+        Cuan Tracker
+      </span>
 
       <div className="mt-10">
-         {menuList.map((menu,index)=> (
-            <h2 className="flex gap-2 items-center text-gray-500 font-medium p-6 cursor-pointer rounded-md hover:text-primary hover:bg-blue-200">
-                <menu.Icon/>
-                {menu.name}
+        {menuList.map((menu, index) => (
+          <Link href={menu.path}>
+            <h2
+              className={`flex gap-2 items-center text-gray-500 font-medium mb-2 p-5 cursor-pointer rounded-md hover:text-primary hover:bg-blue-200 
+                ${path == menu.path && "text-primary bg-blue-200"}
+                `}
+            >
+              <menu.Icon />
+              {menu.name}
             </h2>
-         ))}
+          </Link>
+        ))}
       </div>
       <div className="fixed bottom-10 p-5 flex gap-2 items-center">
-        <UserButton/>
+        <UserButton />
         Profile
       </div>
     </div>
-  )
+  );
 }
 
-export default SideNav
+export default SideNav;
